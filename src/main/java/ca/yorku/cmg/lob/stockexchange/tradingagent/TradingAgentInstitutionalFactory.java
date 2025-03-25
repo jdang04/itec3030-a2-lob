025b8c41b5b1d4a0edf9376f2d967c770a04e3f3
jdang04 <jdang04@my.yorku.ca>
2025-03-25 04:24:39 -0400

    Create TradingAgentInstitutionalFactory.java

---
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentInstitutionalFactory.java
@@ -0,0 +1,18 @@
+package ca.yorku.cmg.lob.stockexchange.tradingagent;
+
+import ca.yorku.cmg.lob.stockexchange.StockExchange;
+import ca.yorku.cmg.lob.stockexchange.events.NewsBoard;
+import ca.yorku.cmg.lob.trader.Trader;
+
+public class TradingAgentInstitutionalFactory extends AbstractTradingAgentFactory {
+
+    public TradingAgent createAgent(String type, String style, Trader t, StockExchange e, NewsBoard n) {
+        ITradingStrategy strategy;
+        if ("Conservative".equalsIgnoreCase(style)) {
+            strategy = new ConservativeStrategy(t, e, n);
+        } else {
+            strategy = new AggressiveStrategy(t, e, n);
+        }
+        return new TradingAgentInstitutional(t, e, n, strategy); // Make sure to pass the strategy here
+    }
+}
